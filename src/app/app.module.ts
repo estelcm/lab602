a2990a8835f8941fdc8a7dce3cd1489c9cde23cc
Estel cm <estel_927@hotmail.com>
2023-02-21 17:46:22 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { IntroductionComponent } from './components/introduction/introduction.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    IntroductionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
